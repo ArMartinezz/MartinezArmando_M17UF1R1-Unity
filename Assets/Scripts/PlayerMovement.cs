5ae93ebfb36b85f82a1b6049202cd06a59f42848
Armando Martinez <contact@amartinez.xyz>
2023-10-26 15:01:27 +0200

    Movidas

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Vector2 boxSize;
+    public float gravity;
+    public float speed;
+    private float hInput;
+    // Components
+    public Rigidbody2D body;
+    public Collider2D playerCollider;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = -9.8f;
+        speed = 5f;
+        body = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Movement
+        hInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(hInput * speed, IsGrounded() ? 0.0f : gravity);
+
+        if (Input.GetKeyDown(KeyCode.V) && IsGrounded()){ ChangeGravity(); }
+    }
+
+    private bool IsGrounded() 
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, gravity < 0 ? Vector2.down : Vector2.up, 0.05f, groundLayer);
+        print(raycastHit.collider != null ? raycastHit.collider : "") ;
+        return raycastHit.collider != null;
+    }
+
+    public void ChangeGravity(){
+        gravity = -gravity;
+        transform.localScale = new Vector3(transform.localScale.x, -transform.localScale.y, transform.localScale.z);
+    }
+
+}
